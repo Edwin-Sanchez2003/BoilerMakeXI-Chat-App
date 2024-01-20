365a4d577f260a0d823c49b4b33f84e60d00e02c
edwsanch <edwsanch@iu.edu>
2024-01-20 15:10:17 -0500

    got basic server running...

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/*
+    Router for the Index Page of the E-Commerce Site
+*/
+
+
+// Imports
+const express = require('express');
+const router = express.Router();
+
+
+// Main Page Route
+router.get('/', async (req, res) => {   
+    res.render('index');
+}); // end get req/resp for main page route
+
+
+// Exports
+module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,51 @@
+// Imports
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const expressLayouts = require('express-ejs-layouts');
+//const methodOverride = require('method-override');
+//const bodyParser = require('body-parser');
 
+
+// construct an express js app
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/status', (request, response) => response.json({clients: clients.length}));
+// Routers
+const indexRouter = require('./routes/index');
 
-const PORT = 3000;
 
-let clients = [];
-let facts = [];
+// Settings
+app.set('view engine', 'ejs'); // use ejs rendering
+app.set('views', __dirname + '/views'); // set the views directory path (for routes)
+app.set('layout', 'layouts/layout'); // set layouts directory
+app.use(expressLayouts);
+app.use(express.static('public')); // create a static files folder
 
-app.listen(PORT, () => {
-  console.log(`Facts Events service listening at http://localhost:${PORT}`)
-})
+
+// Database connection
+/* DB for later...
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose!'))
+*/
+
+
+// Routes
+app.use('/', indexRouter); // homepage router
+/*
+app.use('/events', eventsRouter)
+app.use('/members', membersRouter)
+app.use('/projects', projectsRouter)
+app.use('/resources', resourcesRouter)
+app.use('/admin', adminRouter)
+*/
+
+// If page does not exist:
+app.get('*', (req, res) => {
+    res.render('404');
+});
+
+
+// Listen for requests
+app.listen(process.env.PORT || 5000);
